597db343d7ea162b1ad7d18090ace3f52ca4989d
tobicg <tobicareaga@gmail.com>
2021-10-15 05:04:45 -0300

    entrega final

--- main.js
+++ main.js
@@ -1,73 +1,174 @@
-class Producto {
-    constructor(tipo, precio, talle, color) {
-        this.tipo  = tipo;
-        this.precio  = parseFloat(precio);
-        this.talle = talle;
-        this.color = color;
+$(document).ready(function(){
+
+    $("#openCarrito").click(function(){
+        $("#carrito").css('opacity','1')
+        $("#carrito").css('transform','scale(1)')
+    })
+
+    $("#closeCarrito").click(function(){
+        $("#carrito").css('opacity','0')
+        $("#carrito").css('transform','scale(0)')
+    })
+    
+    $("#comprar").click(function(){
+        $("#carrito").css('opacity','0')
+        $("#carrito").css('transform','scale(0)')
+        $("#compra-completada").css('opacity','1')
+        $("#compra-completada").css('transform','scale(1)')
+    })
+
+    $("#closeCompra").click(function(){
+        $("#compra-completada").css('opacity','0')
+        $("#compra-completada").css('transform','scale(0)')
+    })
+})
+
+const cards = document.getElementById('cards')
+const items = document.getElementById('items')
+const footer = document.getElementById('footer')
+const cartel = document.getElementById('compra-completada')
+const templateCard = document.getElementById('template-card').content
+const templateFooter = document.getElementById('template-footer').content
+const templateCarrito = document.getElementById('template-carrito').content
+const fragment = document.createDocumentFragment()
+
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'))
+        rellenarCarrito()
+    }
+})
+
+cards.addEventListener('click', e => {
+    addCarrito(e)
+})
+
+items.addEventListener('click', e => {
+    accion(e)
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('items.json')
+        const data = await res.json()
+        rellenarCard(data)
+        
+    } catch (error) {
+        console.log (error)
     }
 }
 
-const remeraSBV = new Producto("Remera", 900, "S", "Blanco");
-const remeraMBV = new Producto("Remera", 900, "M", "Blanco");
-const remeraLBV = new Producto("Remera", 900, "L", "Blanco");
+const rellenarCard = data => {
+    data.forEach(producto => {
+        templateCard.querySelector('img').setAttribute("src", producto.img)
+        templateCard.querySelector('#tipo').textContent = producto.tipo
+        templateCard.querySelector('b').textContent = producto.title
+        templateCard.querySelector('#color').textContent = producto.color
+        templateCard.querySelector('#precio').textContent = producto.precio
+        templateCard.querySelector('#btn').dataset.id = producto.id
 
-const remeraSBA = new Producto("Remera", 900, "S", "Blanco");
-const remeraMBA = new Producto("Remera", 900, "M", "Blanco");
-const remeraLBA = new Producto("Remera", 900, "L", "Blanco");
-
-const remeraSBeM = new Producto("Remera", 900, "S", "Beige");
-const remeraMBeM = new Producto("Remera", 900, "M", "Beige");
-const remeraLBeM = new Producto("Remera", 900, "L", "Beige");
-
-const gorraB = new Producto("Gorra", 600, "Único", "Blanca");
-const gorraBe = new Producto("Gorra", 600, "Único", "Beige");
-const gorraN = new Producto("Gorra", 600, "Único", "Negra");
-const gorraV = new Producto("Gorra", 600, "Único", "Verde");
-
-const bolsaB = new Producto("Bolsa", 700, "Único", "Blanca");
-const bolsaBe = new Producto("Bolsa", 700, "Único", "Beige");
-
-const carrito = []
-
-let total = 0;
-
-let pedido1 = prompt("Desea comprar la remera blanca y verde? (Ingrese si o no)")
-
-if (pedido1.toLowerCase() == "si") {
-    carrito.push(remeraMBV);
-    total = total + remeraMBV.precio;
-    alert ("Se ha añadido el producto a su carro")
-} else if (pedido1.toLowerCase() == "no") {
-    alert ("No se ha añadido el producto a su carro")
-} else if (pedido1.toLowerCase() != "si" || pedido1.toLowerCase() != "no") {
-    alert ("Ingrese una respuesta válida")
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    cards.appendChild(fragment)
 }
 
-let pedido2 = prompt("Desea comprar la gorra negra? (Ingrese si o no)")
-
-if (pedido2.toLowerCase() == "si") {
-    carrito.push(gorraN);
-    total = total + gorraN.precio;
-    alert ("Se ha añadido el producto a su carro")
-} else if (pedido2.toLowerCase() == "no") {
-    alert ("No se ha añadido el producto a su carro")
-} else if (pedido2.toLowerCase() != "si" || pedido2.toLowerCase() != "no") {
-    alert ("Ingrese una respuesta válida")
+const addCarrito = e => {
+    if (e.target.classList.contains('btn')) {
+        setCarrito(e.target.parentElement)
+    }
+    e.stopPropagation()
 }
 
-let pedido3 = prompt("Desea comprar la bolsa beige? (Ingrese si o no)")
+const setCarrito = objeto => {
+    const producto = {
+        id: objeto.querySelector('.btn').dataset.id,
+        title: objeto.querySelector('b').textContent,
+        tipo: objeto.querySelector('#tipo').textContent,
+        precio: objeto.querySelector('#precio').textContent,
+        cantidad: 1 
+    }
 
-if (pedido3.toLowerCase() == "si") {
-    carrito.push(bolsaBe);
-    total = total + bolsaBe.precio;
-    alert ("Se ha añadido el producto a su carro")
-} else if (pedido3.toLowerCase() == "no") {
-    alert ("No se ha añadido el producto a su carro")
-} else if (pedido3.toLowerCase() != "si" || pedido3.toLowerCase() != "no") {
-    alert ("Ingrese una respuesta válida")
+    if (carrito.hasOwnProperty(producto.id)) {
+        producto.cantidad = carrito[producto.id].cantidad + 1
+    }
+
+    carrito[producto.id] = {...producto}
+    rellenarCarrito()
 }
 
-carrito.sort((a,b)=> a.precio - b.precio)
+const rellenarCarrito = () => {
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.title
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.tipo
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+        templateCarrito.querySelector('span').textContent = producto.cantidad * producto.precio
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 
-alert(`El total de su compra es $${total}`);
-console.log(carrito);
+    rellenarFooter()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+const rellenarFooter = () => {
+    footer.innerHTML = ''
+    if(Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">No hay productos en su carrito</th>
+        `
+        return
+    }
+
+    const nCantidad = Object.values(carrito).reduce((acc, {cantidad}) => acc + cantidad,0)
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio,0)
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad
+    templateFooter.querySelector('span').textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const eliminarCarrito = document.getElementById('vaciar-carrito')
+    eliminarCarrito.addEventListener('click', () => {
+        carrito = {}
+        rellenarCarrito()
+    })
+
+    const comprar = document.getElementById('comprar')
+    comprar.addEventListener('click', () => {
+        carrito = {}
+        rellenarCarrito()
+    })
+}
+
+const accion = e => {
+    if(e.target.classList.contains('btn-info')){
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad++
+        carrito[e.target.dataset.id] = {...producto}
+
+        rellenarCarrito()
+    }
+
+    if(e.target.classList.contains('btn-danger')){
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id]
+        }
+
+        rellenarCarrito()
+    }
+
+    e.stopPropagation()
+}
